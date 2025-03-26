781c1945e829775fdae5d8d43d58e883c4ce660f
MANJU-ADUR <manjunathadur2001@gmail.com>
2025-03-26 10:25:38 +0530

    MinOperations file

---
+++ src/main/java/org/example/MinOperations.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class MinOperations {
+
+//    public static int minOperations(int[][] grid, int x) {
+//
+//    }
+
+    public static int count(int n1, int n2, int x) {
+        int small = Math.min(n1, n2);
+        int greater = Math.max(n1, n2);
+
+        int diff = greater - small;
+        if (x % 2 == 0 && diff % 2 != 0) {
+            return -1;
+        }
+        if (x % 2 != 0 && diff % 2 == 0) {
+            return -1;
+        }
+        int c = 0;
+        while (small < greater) {
+            System.out.println("small "+small);
+            small += x;
+            c++;
+            if (small == greater) {
+                return c;
+            }
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(6,8,2));
+    }
+}
